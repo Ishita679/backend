c5f02dd1ecb62e5c9258ecc10288312dbfc34770
Ishita Agrawal <ishuagrawal864@gmail.com>
2025-10-23 21:43:22 +0530

    basic

--- index.js
+++ index.js
@@ -1,1 +1,25 @@
-console.log("hi");
+require("dotenv").config(); // Load environment variables from .env file
+
+const express = require("express"); // Import the Express library
+const app = express(); // Create an instance of an Express application
+const port = 8080; // Define the port number
+app.get("/", (req, res) => {
+  res.send("Hello World!");
+});
+
+app.get("/twitter", (req, res) => {
+  res.send("This is Twitter route");
+});
+
+app.get("/login", (req, res) => {
+  res.send("<h1>Login Page</h1>");
+});
+app.listen(port, () => {
+  console.log(`Server is running on port ${port}`);
+});
+app.listen(port, () => {
+  console.log(`Server is running at http://localhost:${port}`);
+});
+app.listen(process.env.PORT, () => {
+  console.log(`Server is running on port ${port}`);
+});
